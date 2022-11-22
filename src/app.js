7196800eff8d4a29282c3a026b77bbad03e45c89
https://github.com/khanhnguyentuann/first-repository.git <khanh.nguyentuann@gmail.com>
2022-11-22 22:37:42 +0700

    Define routes for managing contacts

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-
+const express = require("express");
+const cors = require("cors");
+const contactController = require("./controllers/contact.controller");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({ message: 'Welcom to website selling shoes.' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcom to website selling shoes." });
 });
+
+app
+  .route("/api/contacts")
+  .get(contactController.findAll)
+  .post(contactController.create)
+  .delete(contactController.deleteAll);
+//app.route("/api/contacts/favorite").get(contactController.findAllFavorite);
+app
+  .route("/api/contacts/:id(\\d+)")
+  .get(contactController.findOne)
+  .put(contactController.update)
+  .delete(contactController.delete);
 
 module.exports = app;
